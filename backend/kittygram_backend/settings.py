e5335c79b2d129c7e6982919e3d073c0e0fcfb90
Khamzaev <khamzaew.j@gmail.com>
2025-05-08 12:50:51 +0300

    badge

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -106,6 +106,7 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
